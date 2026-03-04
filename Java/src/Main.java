5f7d53497676f5b378f1d48a5bb416dba518e629
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2026-03-04 10:52:45 +0100

    Personajes Hugo

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,2 +1,4 @@
+import Personajes.Trump;
+
 public class Main {
 }
---
+++ Java/src/Personajes/Morf.java
@@ -0,0 +1,19 @@
+package Personajes;
+
+public abstract class Morf extends Personaje {
+
+    public Morf(String nombre, int vida, int vidaMax) {
+        super("Morf",5, 5);
+    }
+
+    @Override
+    public int ataque() {
+        System.out.println(getNombre() + " ha atacado y ha hecho 5 de daño.");
+        return 5;
+    }
+
+    @Override
+    public void defensa() {
+        System.out.println(getNombre() + " ha drogado al rival");
+    }
+}
