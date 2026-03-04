8815c427cb765f5b9276937bf7c344798affb5c1
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2026-03-04 09:22:34 +0100

    Personaje comun

--- Java/src/Personajes/Personaje.java
+++ Java/src/Personajes/Personaje.java
@@ -1,14 +1,18 @@
 package Personajes;
 
 public class Personaje {
+    private String nombre;
     private int ataque;
     private int defensa;
     private int vida;
+    private int vidaMax;
 
-    public Personaje(int ataque, int defensa, int vida) {
+    public Personaje(int ataque, int defensa, String nombre, int vida, int vidaMax) {
         this.ataque = ataque;
         this.defensa = defensa;
+        this.nombre = nombre;
         this.vida = vida;
+        this.vidaMax = vidaMax;
     }
 
     public int getAtaque() {
@@ -25,10 +29,24 @@ public class Personaje {
         this.defensa = defensa;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public int getVida() {
         return vida;
     }
     public void setVida(int vida) {
         this.vida = vida;
+    }
+
+    public int getVidaMax() {
+        return vidaMax;
+    }
+    public void setVidaMax(int vidaMax) {
+        this.vidaMax = vidaMax;
     }
 }
