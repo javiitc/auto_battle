570081842ceee0c14877327446af2e4f9810ce75
karlosruizmo-stack <karlosruizmo@gmail.com>
2026-03-04 10:52:29 +0100

    .

---
+++ Java/src/Personajes/CarlosKirk.java
@@ -0,0 +1,19 @@
+package Personajes;
+
+public abstract class CarlosKirk extends Personaje {
+
+    public CarlosKirk(String nombre, int vida, int vidaMax) {
+        super("CarlosKirk", 10, 10);
+    }
+
+    @Override
+    public int ataque() {
+        return 5;
+    }
+
+    @Override
+    public void defensa() {
+        System.out.println("Esquiva el ataque y la bala");
+
+    }
+}
