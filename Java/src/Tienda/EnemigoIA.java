e755df07a1ddc41c4a891c9838783308b0f5ed8e
Lemsito22 <lemquilca22@gmail.com>
2026-03-11 10:36:41 +0100

    .

---
+++ Java/src/Tienda/EnemigoIA.java
@@ -0,0 +1,60 @@
+package Tienda;
+
+import Personajes.Personaje;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class EnemigoIA {
+    private ArrayList<Personaje> inventario; // Máximo 10 personajes
+    private ArrayList<Personaje> equipoCombate; // Los 5 que salen a pelear
+    private Random random;
+    private final int MAX_INV = 10;
+
+    public EnemigoIA() {
+        this.inventario = new ArrayList<>();
+        this.equipoCombate = new ArrayList<>();
+        this.random = new Random();
+    }
+
+
+    public void comprarDeTienda(Tienda tienda) {
+        tienda.hacerRoll(); //Llamamos al metodo para obtener personajes que hicieron Juanma y Lex
+
+        // Obtenemos la lista actual de la tienda
+        List<Personaje> slotsDisponibles = tienda.getSlotsPersonajes(); //
+
+        int limiteCompra = Math.min(3, slotsDisponibles.size());
+
+        for (int i = 0; i < limiteCompra; i++) {
+            if (inventario.size() < MAX_INV) {
+                // Cogemos el personaje del slot correspondiente (0, 1 o 2) NO ESCOGEMOS AL AZAR SINO LOS TRES PRIMEROS
+                Personaje p = slotsDisponibles.get(i);
+                if (p != null) {
+                    inventario.add(p);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Elige 5 aleatorios de su inventario para la pelea.
+     */
+    public void seleccionarEquipoCombate() {
+        equipoCombate.clear();
+        ArrayList<Personaje> copia = new ArrayList<>(this.inventario);
+
+        int aElegir = Math.min(5, copia.size());
+
+        for (int i = 0; i < aElegir; i++) {
+            int indice = random.nextInt(copia.size());
+            equipoCombate.add(copia.remove(indice)); // Selección manual sin repetición
+        }
+        System.out.println("IA: Alineación lista con " + equipoCombate.size() + " personajes.");
+    }
+
+    public ArrayList<Personaje> getEquipoCombate() {
+        return equipoCombate;
+    }
+}
