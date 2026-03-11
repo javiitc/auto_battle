489926caa73eded72c0c353ed48c36559f2d9e8a
Lemsito22 <lemquilca22@gmail.com>
2026-03-11 08:35:42 +0100

    Update IaCondiciones.java

--- Java/src/IaSegundoJugador/IaCondiciones.java
+++ Java/src/IaSegundoJugador/IaCondiciones.java
@@ -1,4 +1,6 @@
 package IaSegundoJugador;
 
 public class IaCondiciones {
+    // Tienda de MADURO SURVIVAL
+    // https://github.com/karlosruizmo-stack/juegosurvival/blob/main/game/src/tienda.java
 }
