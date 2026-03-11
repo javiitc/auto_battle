24b8e2aba22a07c837a66fde445bb655d91e55cb
bertaarenas <berta.arenas2007@gmail.com>
2026-03-11 09:34:21 +0100

    .

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,8 +1,15 @@
 import Personajes.Castor;
 import Personajes.Trump;
-
 public class Main {
     static void main(String[] args) {
 
+        System.out.println("-------------------------------");
+        System.out.println("-- BIENVENIDO AL AUTO BATTLE --");
+        System.out.println("-------------------------------");
+
+        System.out.println(" | MENU |");
+        System.out.println("1. ");
+        System.out.println("2. ");
+        System.out.println("3. Salir");
     }
 }
