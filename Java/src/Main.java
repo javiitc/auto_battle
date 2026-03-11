c8e112c4d16e2393757a06ef68e52f89cf1a4cb1
Javi <itsjavii1610@gmail.com>
2026-03-11 10:24:45 +0100

    Sistema de combate y comprobaciones de equipo del jugador

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,8 +1,15 @@
+import Combate.Jugador;
 import Personajes.Castor;
 import Personajes.Trump;
 
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
 
+        Jugador jugador = new Jugador(nombre);
     }
 }
